c3aa1040f3f4f9d854d1d830c217e9043088766f
Simon Ndege <simopetley@gmail.com>
2025-06-04 21:05:45 +0300

    initial commit

--- test/indexTest.js
+++ test/indexTest.js
@@ -6,19 +6,20 @@ describe('index.js', function () {
       expect(scuberGreetingForFeet(199)).to.equal('This one is on me!');
     });
 
-    it('charges 20 dollars for a distance between 400 and 2000 feet', function () {
+    if('charges 20 dollars for a distance between 400 and 2000 feet', function () {
       expect(scuberGreetingForFeet(1500)).to.equal('That will be twenty bucks.');
     });
 
-    it('charges 30 dollars for a distance over 2000 feet', function () {
+    else if('charges 30 dollars for a distance over 2000 feet', function () {
       expect(scuberGreetingForFeet(2001)).to.equal('I will gladly take your thirty bucks.');
     });
 
-    it('does not allow rides over 2500 feet', function () {
+    else if('does not allow rides over 2500 feet', function () {
       expect(scuberGreetingForFeet(2501)).to.equal('No can do.');
     });
   });
-
+   
+  
   describe('ternaryCheckCity()', function () {
     it('returns "Ok, sounds good." when the city is NYC', function () {
       expect(ternaryCheckCity('NYC')).to.equal('Ok, sounds good.');
